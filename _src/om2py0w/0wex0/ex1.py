c56007d6fd37f86604781339720575c1c7a8cff1
wangzhipeng <wangzhipeng@didichuxing.com>
2015-12-01 20:41:37 +0800

    学习

---
+++ _src/om2py0w/0wex0/ex1.py
@@ -0,0 +1,8 @@
+
+print "Hello World 我"
+print "hello Again"
+print "I like typing this"
+print "This is fun"
+print "Yay! Printing"
+print "I d much reather you not"
+print "I  said do not touch this"
